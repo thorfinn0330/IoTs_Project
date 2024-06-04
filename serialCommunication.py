fbdde33a1949f2802460b546b10953e9ac4b485d
LAPTOP-K2DUVB61\PC <tho.nguyenhuu03@gmail.com>
2024-06-04 12:26:41 +0700

    Update serial communication

---
+++ serialCommunication.py
@@ -0,0 +1,72 @@
+import time
+import serial.tools.list_ports
+from constants import*
+import random
+class serialCommunication:
+    def __init__(self) -> None:
+        pass
+        self.portName = self.getPort()
+        try:
+            self.ser = serial.Serial(port=self.portName, baudrate=9600)
+            print("Open successfully")
+        except:
+            print("Can not open the port")
+    
+    def getPort(self):
+        ports = serial.tools.list_ports.comports()
+        N = len(ports)
+        commPort = "None"
+        for i in range(0, N):
+            port = ports[i]
+            strPort = str(port)
+            if "USB" in strPort:
+                splitPort = strPort.split(" ")
+                commPort = (splitPort[0])
+        return commPort
+        # return "/dev/ttyUSB1"
+
+    def readData(self):
+        bytesToRead = self.ser.inWaiting()
+        if bytesToRead > 0:
+            out = self.ser.read(bytesToRead)
+            data_array = [b for b in out]
+            print("Array:", data_array)
+            if len(data_array) >= 7:
+                array_size = len(data_array)
+                value = data_array[array_size - 4] * 256 + data_array[array_size - 3]
+                return value
+            else:
+                return -1
+        return 0
+
+    def setDevice(self, id, state):
+        if state == True:
+            print(relay_ON[id], "--------")
+            self.ser.write(relay_ON[id])
+        else:
+            print(relay_OFF[id], "--------")
+            self.ser.write(relay_OFF[id])
+        
+        print("Result: ", self.readData())
+
+    def readTemperature(self):
+        return random.randint(20,40)
+        self.readData()
+        self.ser.write(soil_temperature)
+        time.sleep(1) 
+        #change to task
+        return self.readData()
+
+    def readMoisture(self):
+        return random.randint(50,80)
+        self.readData()
+        self.ser.write(soil_moisture)
+        time.sleep(1) 
+        #change to task
+        return self.readData()   
+    
+    def readAllSensors(self):
+        return self.readTemperature(), self.readMoisture()
+    
+
+ser = serialCommunication() 
